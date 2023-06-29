5b71facf5890608cfa23dc5ba14b619464eaba6b
BAEUNJI <130124068+BAEUNJI@users.noreply.github.com>
2023-06-29 19:54:46 +0900

    채소 까지 추가한 코드인데.. 에러는 없지만, 검색탭에서 계속 오류나는 상태 ㅜㅜ (코드 에러는 없음)

--- app/src/main/java/com/example/myapplication/bottomnavigation/FragSearch.java
+++ app/src/main/java/com/example/myapplication/bottomnavigation/FragSearch.java
@@ -1,11 +1,11 @@
 package com.example.myapplication.bottomnavigation;
 
 
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,16 +14,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
-import android.content.Context;
-import android.database.Cursor;
+
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -90,6 +86,8 @@ public class FragSearch extends Fragment {
     }
 
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
@@ -97,26 +95,32 @@ public class FragSearch extends Fragment {
 
 
         //api에서 데이터 가져오고 파싱
+        List<String> categoryCodes = Arrays.asList("400", "200"); // 카테고리 코드
+        List<Map<String, String>> fruits = new ArrayList<>(); // 과일 정보를 저장할 리스트
+        List<Map<String, String>> veges = new ArrayList<>(); // 채소 정보를 저장할 리스트
 
-        new Thread(() -> {
+
+
+        for (String code : categoryCodes) {
             String url_text = "https://www.kamis.or.kr/service/price/xml.do?action=dailyPriceByCategoryList&p_product_cls_code=02&p_country_code=1101&" +
                     "p_regday=2023-06-23&" +
                     "p_convert_kg_yn=N&" +
-                    "p_item_category_code=400&" +
+                    "p_item_category_code=" + code + "&" +
                     "p_cert_key=" + API_KEY + "&p_cert_id=222&p_returntype=xml";
-            String text = getText(url_text);
 
+
+
+
+            String text = getText(url_text);
             Document doc = convertStringToDocument(text);
 
-            //필요한정보 뽑아내는 코드 구현;
-
+            // 필요한 정보 뽑아내는 코드 구현
             NodeList nList = doc.getElementsByTagName("item");
-            System.out.println("파싱할 리스트 수 : "+ nList.getLength());  // 파싱할 리스트 수 :  5
+            System.out.println("파싱할 리스트 수 : "+ nList.getLength());
 
-            for(int temp = 1; temp < nList.getLength(); temp++){
+            for(int temp = 0; temp < nList.getLength(); temp++){
                 Node nNode = nList.item(temp);
                 if(nNode.getNodeType() == Node.ELEMENT_NODE){
-
                     Element eElement = (Element) nNode;
                     if (getTagValue("rank",eElement).contains("중품") || getTagValue("rank",eElement).contains("M과")) continue;
                     System.out.println("######################");
@@ -126,38 +130,81 @@ public class FragSearch extends Fragment {
                     System.out.println("1일 전 가격: " + getTagValue("dpr2", eElement));
                     System.out.println("1주일 전 가격: " + getTagValue("dpr3", eElement));
 
-                    Map<String, String> fruit_info = new HashMap<String, String>();
-                    fruit_info.put("item_name", getTagValue("item_name", eElement));
-                    fruit_info.put("item_code", getTagValue("item_code", eElement));
-                    fruit_info.put("dpr1", getTagValue("dpr1", eElement));
-                    fruit_info.put("dpr2", getTagValue("dpr2", eElement));
-                    fruit_info.put("dpr3", getTagValue("dpr3", eElement));
-                    fruits.add(fruit_info);
-                }	// for end
-            }	// if end
-            b = false;
+                    Map<String, String> info = new HashMap<>();
+                    info.put("item_name", getTagValue("item_name", eElement));
+                    info.put("item_code", getTagValue("item_code", eElement));
+                    info.put("dpr1", getTagValue("dpr1", eElement));
+                    info.put("dpr2", getTagValue("dpr2", eElement));
+                    info.put("dpr3", getTagValue("dpr3", eElement));
 
-        }).start();
+                    if (code.equals("400")) {
+                        fruits.add(info);
+                    } else if (code.equals("200")) {
+                        veges.add(info);
+                    }
 
-        while(b){}
-        System.out.println(fruits);
+                }   // if end
+            }   // for end
+        } // for end
+
+        System.out.println("Fruits: " + fruits);
+        System.out.println("Vegetables: " + veges);
+
 
         List<Integer> drawables = Arrays.asList(R.drawable.apple, R.drawable.pear,
                 R.drawable.mandarin, R.drawable.banana, R.drawable.kiwi, R.drawable.pineapple,
-                R.drawable.orange, R.drawable.lemon, R.drawable.cherry, R.drawable.mango);
+                R.drawable.orange, R.drawable.lemon, R.drawable.cherry, R.drawable.mango,
+                R.drawable.kimchicabbage, R.drawable.cabbage, R.drawable.spinach, R.drawable.lettuce,
+                R.drawable.wintercabbage, R.drawable.watermelon, R.drawable.orientalmelon);
+
+
+
 
         //===== 테스트를 위한 더미 데이터 생성 ===================
         ArrayList<DataModel> testDataSet = new ArrayList<>();
 
-        for(int i=0; i<drawables.size(); i++){
-            String item_name = fruits.get(i).get("item_name");
 
+        // For fruits
+        for(int i=0; i<fruits.size(); i++){
+            String item_name = fruits.get(i).get("item_name");
             String dpr1 = fruits.get(i).get("dpr1");
             String dpr3 = fruits.get(i).get("dpr3");
 
-            testDataSet.add(new DataModel(item_name, drawables.get(i), dpr1, dpr3));
+            int drawableId;
+            drawableId = R.drawable.apple;
+
+            if(i < drawables.size()){
+                drawableId = drawables.get(i);
+            } else {
+                // Default drawableId if there are not enough drawables
+                drawableId = R.drawable.default_image;
+            }
+
+            testDataSet.add(new DataModel(item_name, drawableId, dpr1, dpr3));
+
 
         }
+
+        // For vegetables
+        for(int i=0; i<veges.size(); i++){
+            String item_name = veges.get(i).get("item_name");
+            String dpr1 = veges.get(i).get("dpr1");
+            String dpr3 = veges.get(i).get("dpr3");
+
+            int drawableId;
+            drawableId = R.drawable.default_vege;  // you may need a different default image for vegetables
+
+            if(i < drawables.size()){
+                drawableId = drawables.get(i);
+            } else {
+                // Default drawableId if there are not enough drawables
+                drawableId = R.drawable.default_image;
+            }
+
+            testDataSet.add(new DataModel(item_name, drawableId, dpr1, dpr3));
+        }
+
+
 
 
 
@@ -210,11 +257,6 @@ public class FragSearch extends Fragment {
         });
         return view;
     }
-
-
-
-
-
 
 
 
